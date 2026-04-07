b977044ea1a67aa40b013f682454ae8336035023
b22thohe <b22thohe@student.his.se>
2026-04-07 08:51:28 +0200

    add some sprites

--- Timber.cpp
+++ Timber.cpp
@@ -2,6 +2,11 @@
 
 using namespace sf;
 
+const float TREE_H_POS = 810;
+const float TREE_V_POS = 0;
+const float BEE_H_POS = 0;
+const float BEE_V_POS = 800;
+
 // This is where the game starts from int main()
 int main() {
 	// Create a video mode object
@@ -25,6 +30,26 @@ int main() {
 	// Set sprite to cover the screen
 	spriteBackground.setPosition(0, 0);
 	
+	// Make a tree sprite
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(TREE_H_POS, TREE_V_POS);
+	
+	// Prepare the bee sprite
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(BEE_H_POS, BEE_V_POS);
+	
+	// Set bee activity
+	bool beeActive = false;
+	
+	// Set bee movement speed
+	float beeSpeed = 0.0f;
+	
 	while (window.isOpen()) {
 		// Handle Player input
 		
@@ -41,6 +66,9 @@ int main() {
 		window.clear();
 		
 		// Draw game scene
+		window.draw(spriteBackground);
+		window.draw(spriteTree);
+		window.draw(spriteBee);
 		
 		// Show everything we just drew
 		window.display();
