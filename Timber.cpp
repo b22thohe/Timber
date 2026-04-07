4c6ee782b85f00fb5629f3a894737cd35024f366
b22thohe <tomhell66@gmail.com>
2026-04-07 11:13:31 +0200

    Add clouds

--- Timber.cpp
+++ Timber.cpp
@@ -6,6 +6,9 @@ const float TREE_H_POS = 810;
 const float TREE_V_POS = 0;
 const float BEE_H_POS = 0;
 const float BEE_V_POS = 800;
+const float CLOUD1_V_POS = 0;
+const float CLOUD2_V_POS = 250;
+const float CLOUD3_V_POS = 500;
 
 // This is where the game starts from int main()
 int main() {
@@ -49,6 +52,34 @@ int main() {
 	
 	// Set bee movement speed
 	float beeSpeed = 0.0f;
+
+	// Make some clouds
+	Texture textureCloud;
+	textureCloud.loadFromFile("graphics/cloud.png");
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+
+	// Position clouds left on screen at different heights
+	spriteCloud1.setPosition(0, CLOUD1_V_POS);
+	spriteCloud2.setPosition(0, CLOUD2_V_POS);
+	spriteCloud3.setPosition(0, CLOUD3_V_POS);
+
+	// Set cloud activity
+	bool cloud1Active = false;
+	bool cloud2Active = false;
+	bool cloud3Active = false;
+
+	// Set cloud movement speed
+	float cloud1Speed = 0.0f;
+	float cloud2Speed = 0.0f;
+	float cloud3Speed = 0.0f;
+
+	// Time control variables
+	Clock clock;
 	
 	while (window.isOpen()) {
 		// Handle Player input
@@ -67,6 +98,9 @@ int main() {
 		
 		// Draw game scene
 		window.draw(spriteBackground);
+		window.draw(spriteCloud1);
+		window.draw(spriteCloud2);
+		window.draw(spriteCloud3);
 		window.draw(spriteTree);
 		window.draw(spriteBee);
 		
