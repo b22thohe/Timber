07528be7c6ec91faa7f988ee16a8461696d7cb4f
b22thohe <tomhell66@gmail.com>
2026-04-10 12:21:47 +0200

    Add PC, axe and movement detection

--- Timber.cpp
+++ Timber.cpp
@@ -3,6 +3,13 @@
 
 using namespace sf;
 
+/* PROTOTYPES */
+void centerText(Text& text, Vector2f position);
+
+void updateCloud(Sprite& cloud, float& speed, bool& active, float dt, int screenWidth);
+
+void updateBranches(int seed);
+
 const float TREE_H_POS = 810;
 const float TREE_V_POS = 0;
 const float BEE_H_POS = 0;
@@ -10,14 +17,13 @@ const float BEE_V_POS = 800;
 const float CLOUD1_V_POS = 0;
 const float CLOUD2_V_POS = 250;
 const float CLOUD3_V_POS = 500;
+const int NUM_BRANCHES = 6;
 
 enum class State { PAUSED, PLAYING, GAME_OVER, START_SCREEN };
+enum class Side { LEFT, RIGHT, NONE };
 
-/* PROTOTYPES */
-void centerText(Text& text, Vector2f position);
-
-void updateCloud(Sprite& cloud, float& speed, bool& active, float dt, int screenWidth);
-
+Sprite branches[NUM_BRANCHES];
+Side branchPositions[NUM_BRANCHES];
 
 // This is where the game starts from int main()
 int main() {
@@ -156,67 +162,142 @@ int main() {
 
 	State gameState = State::START_SCREEN;
 
+	// Prepare branches
+	Texture textureBranch;
+	textureBranch.loadFromFile("graphics/branch.png");
+	// Set texture on branches
+	for (int i = 0; i < NUM_BRANCHES; i++)
+	{
+		branches[i].setTexture(textureBranch);
+		branches[i].setPosition(-2000, -2000);
+
+		// Set sprite's origin to center
+		branches[i].setOrigin(220, 20);
+	}
+
+	// Prepare the player
+	Texture texturePlayer;
+	texturePlayer.loadFromFile("graphics/player.png");
+	Sprite spritePlayer;
+	spritePlayer.setTexture(texturePlayer);
+	spritePlayer.setPosition(580, 720);
+
+	// Player starts on the left
+	Side playerSide = Side::LEFT;
+
+	// Prepare the gravestone
+	Texture textureRIP;
+	textureRIP.loadFromFile("graphics/rip.png");
+	Sprite spriteRIP;
+	spriteRIP.setTexture(textureRIP);
+	spriteRIP.setPosition(600, 860);
+
+	// Prepare the axe
+	Texture textureAxe;
+	textureAxe.loadFromFile("graphics/axe.png");
+	Sprite spriteAxe;
+	spriteAxe.setTexture(textureAxe);
+	spriteAxe.setPosition(700, 830);
+
+	// Line up the axe with the tree
+	const float AXE_POSITION_LEFT = 700;
+	const float AXE_POSITION_RIGHT = 1075;
+
+	// Prepare the flying log
+	Texture textureLog;
+	textureLog.loadFromFile("graphics/log.png");
+	Sprite spriteLog;
+	spriteLog.setTexture(textureLog);
+	spriteLog.setPosition(810, 720);
+
+	// Log related varibales
+	bool logActive = false;
+	float logSpeedX = 1000;
+	float logSpeedY = -1500;
+
+	// Player input control
+	bool acceptInput = false;
+
 	while (window.isOpen()) {
-		/* 1. Handle Events (Better for Toggles) */
+
+		/* 1. Handle Events */
 		Event event;
 		while (window.pollEvent(event)) {
-			if (event.type == Event::Closed)
-				window.close();
+		    if (event.type == Event::Closed)
+		        window.close();
 
-			// Check for a single key press
-			if (event.type == Event::KeyPressed) {
-				if (event.key.code == Keyboard::Space) {
-					switch (gameState)
-					{
-						case State::START_SCREEN:
-							// Move from start state to begin the game
-							gameState = State::PLAYING;
-							break;
+		    // Reset acceptInput when the user lets go of a key
+		    if (event.type == Event::KeyReleased && gameState == State::PLAYING) {
+		        acceptInput = true;
+		        // Hide the axe when they let go
+		        spriteAxe.setPosition(2000, spriteAxe.getPosition().y);
+		    }
 
-						case State::PLAYING:
-							// Move from playing to pause
-							gameState = State::PAUSED;
-							break;
+		    if (event.type == Event::KeyPressed) {
+		        // Handle ESCAPE
+		        if (event.key.code == Keyboard::Escape) window.close();
 
-						case State::PAUSED:
-							// Move from paused to playing
-							gameState = State::PLAYING;
-							break;
+		        // Handle SPACE
+		        if (event.key.code == Keyboard::Space) {
+		            if (gameState == State::START_SCREEN) gameState = State::PLAYING;
+		            else if (gameState == State::PLAYING) gameState = State::PAUSED;
+		            else if (gameState == State::PAUSED)  gameState = State::PLAYING;
+		            // Note: Game Over is handled by Enter or Space depending on preference
+		            else if (gameState == State::GAME_OVER) {
+		                // Quick reset for Space
+		                score = 0;
+		                timerRemaining = 6.0f;
+		                gameState = State::PLAYING;
+		            }
+		        }
 
-						case State::GAME_OVER:
-							// 1. Reset game variables
-							score = 0;
-							timerRemaining = 6.0f;
+		        // Handle ENTER (Hard Reset)
+		        if (event.key.code == Keyboard::Enter) {
+		            gameState = State::START_SCREEN;
+		            messageText.setString("Press <SPACE> to start!");
+		            centerText(messageText, Vector2f(1920 / 2.0f, 1080 / 2.0f));
+		            score = 0;
+		            timerRemaining = 6.0f;
+		            beeActive = false;
+		            cloud1Active = false;
+		            cloud2Active = false;
+		            cloud3Active = false;
+		            for (int i = 0; i < NUM_BRANCHES; i++) branchPositions[i] = Side::NONE;
+		            spriteRIP.setPosition(675, 2000);
+		            spritePlayer.setPosition(580, 720);
+		            acceptInput = true;
+		        }
 
-							// 2. Reset scene objects so they spawn fresh
-							beeActive = false;
-							cloud1Active = false;
-							cloud2Active = false;
-							cloud3Active = false;
+		        /* Handle THE CHOP (Only if playing and input is accepted) */
+		        if (gameState == State::PLAYING && acceptInput) {
 
-							// 3. Change state to start playing again immediately
-							gameState = State::PLAYING;
-							break;
-					}
-				}
-				if (event.key.code == Keyboard::Escape) {
-					window.close();
-				}
-				if (event.key.code == Keyboard::Enter)
-				{
-					gameState = State::START_SCREEN;
-					messageText.setString("Press <SPACE> to start!");
-					centerText(messageText, Vector2f(1920 / 2.0f, 1080 / 2.0f));
+		            if (event.key.code == Keyboard::Right) {
+		                playerSide = Side::RIGHT;
+		                score++;
+		                timerRemaining += (2.0f / score) + 0.15f;
+		                spriteAxe.setPosition(AXE_POSITION_RIGHT, 830);
+		                spritePlayer.setPosition(1200, 720);
+		                updateBranches(score);
+		                spriteLog.setPosition(810, 720);
+		                logSpeedX = -5000;
+		                logActive = true;
+		                acceptInput = false; // Stop further chops until key is released
+		            }
 
-					// 3. Reset game variables
-					score = 0;
-					timerRemaining = 6.0f;
-					beeActive = false;
-					cloud1Active = false;
-					cloud2Active = false;
-					cloud3Active = false;
-				}
-			}
+		            if (event.key.code == Keyboard::Left) {
+		                playerSide = Side::LEFT;
+		                score++;
+		                timerRemaining += (2.0f / score) + 0.15f;
+		                spriteAxe.setPosition(AXE_POSITION_LEFT, 830);
+		                spritePlayer.setPosition(580, 720);
+		                updateBranches(score);
+		                spriteLog.setPosition(810, 720);
+		                logSpeedX = 5000;
+		                logActive = true;
+		                acceptInput = false; // Stop further chops until key is released
+		            }
+		        }
+		    }
 		}
 
 		/* Update scene */
@@ -272,6 +353,33 @@ int main() {
 			updateCloud(spriteCloud1, cloud1Speed, cloud1Active, dt.asSeconds(), 1920);
 			updateCloud(spriteCloud2, cloud2Speed, cloud2Active, dt.asSeconds(), 1920);
 			updateCloud(spriteCloud3, cloud3Speed, cloud3Active, dt.asSeconds(), 1920);
+
+			// Update branches
+			for (int i = 0; i < NUM_BRANCHES; i++)
+			{
+				float height = i * 150;
+
+				if (branchPositions[i] == Side::LEFT)
+				{
+					// Move sprite to left
+					branches[i].setPosition(610, height);
+					// Flip the sprite
+					branches[i].setRotation(180);
+				}
+				else if (branchPositions[i] == Side::RIGHT)
+				{
+					// Move sprite right
+					branches[i].setPosition(1330, height);
+					// Set sprite rotation to normal
+					branches[i].setRotation(0);
+				}
+				else
+				{
+					// If neither left or right --> Hide the branch
+					branches[i].setPosition(3000, height);
+				}
+
+			}
 		}
 		else
 		{
@@ -293,7 +401,24 @@ int main() {
 		window.draw(spriteCloud1);
 		window.draw(spriteCloud2);
 		window.draw(spriteCloud3);
+		for (int i = 0; i < NUM_BRANCHES; i++)
+		{
+			window.draw(branches[i]);
+		}
 		window.draw(spriteTree);
+
+		// Draw the player
+		window.draw(spritePlayer);
+
+		// Draw the axe
+		window.draw(spriteAxe);
+
+		// Draw the flying log
+		window.draw(spriteLog);
+
+		// Draw the tombstone
+		window.draw(spriteRIP);
+
 		window.draw(spriteBee);
 		window.draw(scoreText);
 		window.draw(timerBar);
@@ -337,5 +462,33 @@ void updateCloud(Sprite& cloud, float& speed, bool& active, float dt, int screen
 		if (cloud.getPosition().x > screenWidth) {
 			active = false;
 		}
+	}
+}
+
+// Function for moving branches
+void updateBranches(int seed)
+{
+	// Move all branches down one place
+	for (int j = NUM_BRANCHES-1; j > 0; j--)
+	{
+		branchPositions[j] = branchPositions[j - 1];
+	}
+
+	// Spawn a new branch at pos 0
+	// LEFT, RIGHT, or NONE
+	srand((int)time(0)+seed);
+	int r = (rand() % 5);
+
+	switch (r)
+	{
+	case 0:
+		branchPositions[0] = Side::LEFT;
+		break;
+	case 1:
+		branchPositions[0] = Side::RIGHT;
+		break;
+	default:
+		branchPositions[0] = Side::NONE;
+		break;
 	}
 }
